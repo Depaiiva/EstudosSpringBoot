bea21d89321ffc8d6a0022ed216907495b217640
Carlos Henrique <carlos.rique23@gmail.com>
2025-08-06 20:05:38 -0300

    mapeamento das entidades

---
+++ library/src/main/java/com/depaiva/library/model/Autor.java
@@ -0,0 +1,40 @@
+package com.depaiva.library.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "autor", schema = "public")
+@Getter
+@Setter
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "data_nascimento",  nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+
+    @OneToMany(mappedBy = "autor")
+    private List<Livro> livros;
+
+
+    // @Column - definições:
+    // nullable é a mesma coisa que o not null no SQL
+    // length é o tanto de caracteres varchar no SQL. OBS: só funciona com atributos do tipo String
+    // name é o nome de acordo com as colunas na tabela. Se o nome for o mesmo da coluna respectiva da tabela não precisa especificar aqui
+
+}
---
+++ library/src/main/java/com/depaiva/library/model/Livro.java
@@ -0,0 +1,39 @@
+package com.depaiva.library.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "livro")
+@Data
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "isbn", nullable = false, length = 20)
+    private String isbn;
+
+    @Column(name = "titulo", nullable = false, length = 150)
+    private String titulo;
+
+    @Column(name = "dataPubicacao")
+    private LocalDate dataPubicacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genero", nullable = false)
+    private GeneroLivro genero;
+
+    @Column(name = "preco", nullable = false, precision = 18, scale = 2)
+    private BigDecimal preco;
+
+    @ManyToOne
+    @JoinColumn(name = "id_autor")
+    private Autor autor;
+}
